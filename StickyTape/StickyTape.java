b7295e9774e56c17e1e9c1a4b22c3fa5d973c396
Max <max.hammond21@gmail.com>
2022-11-12 12:00:36 +1100

    added files

---
+++ StickyTape/StickyTape.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * Works similarly to the paste command
+*/
+public class StickyTape {
+
+    public String[] readFile(String f) {
+
+    }
+
+    public static void main(String[] args) {
+
+        String[] words;
+
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+
+    }
+}
